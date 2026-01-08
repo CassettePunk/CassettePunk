5dc6b8298a219ffdef907fc0b04d3ccae03722c7
SpeltIncorrectyl <speltincorrectyl0001@gmail.com>
2026-01-08 01:23:49 +0000

    added reloading

---
+++ Content.Shared/Reloading/Components/ReloadableComponent.cs
@@ -0,0 +1,18 @@
+using Content.Shared.Tag;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.Reloading.Components;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class ReloadableComponent: Component
+{
+    [DataField, AutoNetworkedField]
+    public string Container = string.Empty;
+
+    [DataField, AutoNetworkedField]
+    public ProtoId<TagPrototype> AmmoTag = string.Empty;
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan ReloadTime = TimeSpan.FromSeconds(1);
+}
--- Content.Shared/Reloading/Systems/ReloadingSystem.cs
+++ Content.Shared/Reloading/Systems/ReloadingSystem.cs
@@ -1,12 +1,26 @@
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Content.Shared.Hands.EntitySystems;
 using Content.Shared.Input;
+using Content.Shared.Inventory;
+using Content.Shared.Reloading.Components;
+using Content.Shared.Storage;
+using Content.Shared.Tag;
+using Robust.Shared.Containers;
 using Robust.Shared.Input.Binding;
 using Robust.Shared.Player;
+using Robust.Shared.Utility;
 
 namespace Content.Shared.Reloading.Systems;
 
 public sealed class ReloadingSystem: EntitySystem
 {
+    [Dependency] private readonly InventorySystem _inventory = default!;
+    [Dependency] private readonly SharedContainerSystem _container = default!;
+    [Dependency] private readonly SharedHandsSystem _hands = default!;
+    [Dependency] private readonly TagSystem _tag = default!;
+
     public override void Initialize()
     {
         base.Initialize();
@@ -24,6 +38,53 @@ public sealed class ReloadingSystem: EntitySystem
 
     private void HandleReload(ICommonSession? session)
     {
-        Log.Debug("Reloading!");
+        if (session?.AttachedEntity is null)
+            return;
+        ReloadNow(session.AttachedEntity.Value);
+    }
+
+    private bool ResolveReloadingArgs(ref EntityUid reloader, [NotNullWhen(true)] ref Entity<ReloadableComponent?>? reloadee, [NotNullWhen(true)] ref EntityUid? storage)
+    {
+        if (reloadee is null)
+        {
+            if (!_hands.TryGetHeldItem(reloader, "hand", out var held))
+                return false;
+            reloadee = held;
+        }
+
+        var reloadableComp = reloadee.Value.Comp;
+        if (!Resolve(reloadee.Value.Owner, ref reloadableComp))
+            return false;
+        reloadee = (reloadee.Value.Owner, reloadableComp);
+
+        if (storage is null)
+        {
+            if (!_inventory.TryGetSlotEntity(reloader, "back", out var backpack))
+                return false;
+            storage = backpack;
+        }
+
+        return true;
+    }
+
+    public bool ReloadNow(EntityUid reloader, Entity<ReloadableComponent?>? reloadee = null, EntityUid? storage = null)
+    {
+        if (!ResolveReloadingArgs(ref reloader, ref reloadee, ref storage))
+            return false;
+
+        var replacementContainer = _container.EnsureContainer<Container>(storage.Value, "storagebase");
+        var replacement = replacementContainer.ContainedEntities.FirstOrNull(x => _tag.HasTag(x, reloadee.Value.Comp!.AmmoTag));
+        if (replacement is null)
+            return false;
+
+        var ammoContainer = _container.EnsureContainer<ContainerSlot>(reloadee.Value.Owner, reloadee.Value.Comp!.Container);
+        var ammo = ammoContainer.ContainedEntity;
+        if (ammo is not null)
+            _container.InsertOrDrop(ammo.Value, replacementContainer);
+        _container.Insert(replacement.Value, ammoContainer);
+
+        Log.Debug("reloaded successfully");
+
+        return true;
     }
 }
